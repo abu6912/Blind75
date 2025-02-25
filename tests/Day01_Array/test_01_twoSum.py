a73037fbb99df22110ce23134f909110876a5e42
Deeptangshu Baidya <abu6912@gmail.com>
2025-02-25 06:14:52 +0530

    add unittest for twoSum

--- tests/Day01_Array/test_01_twoSum.py
+++ tests/Day01_Array/test_01_twoSum.py
@@ -5,10 +5,24 @@ from src.Day01_Array.P01_twoSum import Solution
 
 
 class TestTwoSum(unittest.TestCase):
-    def test_simple_case(self):
+    def test_simple_case1(self):
         sol = Solution()
-        nums = [2,7,11,15]
+        nums = [2, 7, 11, 15]
         target = 9
+        output = [0, 1]
+        self.assertCountEqual(sol.twoSum(nums, target), output)
+
+    def test_simple_case2(self):
+        sol = Solution()
+        nums = [3, 2, 4]
+        target = 6
+        output = [1, 2]
+        self.assertCountEqual(sol.twoSum(nums, target), output)
+
+    def test_simple_case3(self):
+        sol = Solution()
+        nums = [3, 3]
+        target = 6
         output = [0,1]
         self.assertCountEqual(sol.twoSum(nums, target), output)
 
